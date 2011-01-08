12e473b541d57bb5ac11c817e5b08468e5fae385
Santoash Rajaram <scuddalo@alumni.cmu.edu>
2011-01-08 01:24:37 -0800

    updating rails version to 2.3.10

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,12 @@
  ENV['RAILS_ENV'] ||= 'development'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.10' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
+require 'lib/rails/rack/metal'
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
