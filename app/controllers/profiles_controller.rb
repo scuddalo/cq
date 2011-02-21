63816021accb172bb7490781bf775828280fe4ce
Santoash Rajaram <scuddalo@alumni.cmu.edu>
2011-02-20 17:06:31 -0800

    add ignore_last_activity_time to unread to support first time fetch

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -83,12 +83,12 @@ class ProfilesController < ApplicationController
   def number_of_unreads
     ignore_last_activity_time = params[:ignoreLastActivity]
     # first fetch all the seek requests 
-    all_seek_requests = requested_profile.active_seek_requests_since_last_push(ignore_last_activity_time)
+    all_seek_requests = requested_profile.active_seek_requests_since_last_push(ignore_last_activity_time == 1)
     seek_req_count = all_seek_requests.count
     
     # second, fetch all seek responses
     active_seek = requested_profile.active_seek
-    all_seek_responses = active_seek.nil? ? Array.new : active_seek.seek_responses_since_last_activity(ignore_last_activity_time)
+    all_seek_responses = active_seek.nil? ? Array.new : active_seek.seek_responses_since_last_activity(ignore_last_activity_time == 1)
     seek_res_count = all_seek_responses.nil? ? 0 : all_seek_responses.count
     
     PushActivity.update_last_activity_time()
