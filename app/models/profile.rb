b759923921a3f347ca49e0d74af3337bec55e77f
Santoash Rajaram <scuddalo@alumni.cmu.edu>
2011-02-20 17:07:35 -0800

    add ignore_last_activity_time to unread to support first time fetch

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -70,7 +70,7 @@ class Profile < ActiveRecord::Base
   end
   
   def active_seek_requests_since_last_push(should_ignore_last_activity_time)
-    if should_ignore_last_activity_time
+    if !should_ignore_last_activity_time
       last_activity = PushActivity.find(:all, :order => "activity_date desc", :limit=> 1)
       last_activity = last_activity.kind_of?(Array) ? last_activity.first : last_activity
       active_seek_requests = SeekRequest.find(:all,  
